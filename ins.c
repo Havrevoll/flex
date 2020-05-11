5095b0a663871cb4c9af99ba123663d8e50870ca
Ola Haugen Havrevoll <havrevoll@gmail.com>
2020-05-11 17:25:03 +0200

    Fyrste

---
+++ ins.c
@@ -0,0 +1,13 @@
+// insertion sort, several errors
+// 
+// usage: insert_sort num1 num2 num3 ..., where the numi are the numbers to
+// be sorted
+//
+//
+
+int x[10], // input array
+    y[10], // workspace array
+    num_inputs,  //length of input array
+    num_y = 0; // current number of elements in y
+
+void get_args(int ac, char **
