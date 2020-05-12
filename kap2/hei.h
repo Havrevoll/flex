b1bf9db7f5452b46c55764c4a92a333c42d47d8d
Ola Haugen Havrevoll <havrevoll@gmail.com>
2020-05-12 09:14:09 +0200

    la til alt frå flexbison-boka

---
+++ kap2/hei.h
@@ -0,0 +1,9 @@
+#include "peter.h"
+
+Jajaja line 3 for line to er tom.
+
+fsdf line fem men kanskje blir sett på som line fire?
+g
+h
+hh
+slutt for hei, på line ni
---
+++ kap2/peter.h
@@ -0,0 +1,9 @@
+dette er peter
+line to i ftrakakja
+tri sdfsfssd
+fira sgh
+hsa fem
+seks
+hahh
+aahaha
+siste lina, ni
